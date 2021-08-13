4c2a4bd44c2b008ee79088d4da1562bdd9831485
eungikim <eungikim.me@gmail.com>
2021-08-13 22:38:20 +0900

    Init proj
    Add asset and show title in recyclerview

---
+++ app/src/main/java/me/eungi/beatbox/MainActivity.kt
@@ -0,0 +1,69 @@
+package me.eungi.beatbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import me.eungi.beatbox.databinding.ActivityMainBinding
+import me.eungi.beatbox.databinding.ListItemSoundBinding
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var beatBox: BeatBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        beatBox = BeatBox(assets)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter(beatBox.sounds)
+        }
+    }
+
+
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.viewModel = SoundViewModel()
+        }
+
+        fun bind(sound: Sound) {
+            binding.apply {
+                viewModel?.sound = sound
+                executePendingBindings()
+            }
+        }
+    }
+
+    private inner class SoundAdapter(private val sounds: List<Sound>) :
+        RecyclerView.Adapter<SoundHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            return SoundHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            holder.bind(sounds[position])
+        }
+
+        override fun getItemCount() = sounds.size
+
+    }
+
+}
---
+++ app/src/main/java/me/eungi/beatbox/SoundViewModel.kt
@@ -0,0 +1,16 @@
+package me.eungi.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel : BaseObservable() {
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+            notifyChange()
+        }
+
+    @get:Bindable
+    val title: String?
+        get() = sound?.name
+}
